b5e0c5c41cc961f35635e5d16c5d889ed7e88a86
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2024-08-18 22:37:24 +0300

    Initial Commit

---
+++ Numio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Numio
+//
+//  Created by Sasha Jaroshevskii on 18.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Numio/NumioApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumioApp.swift
+//  Numio
+//
+//  Created by Sasha Jaroshevskii on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct NumioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
